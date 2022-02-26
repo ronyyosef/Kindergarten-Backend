c8e0dab9a75d7a823aec74a40712bedabc1f2bd5
Dave <keissar3@gmail.com>
2022-02-26 17:49:41 +0200

    fixing param

--- lambdas/children/add_child_data_lambda.py
+++ lambdas/children/add_child_data_lambda.py
@@ -6,11 +6,11 @@ from utils.logger import logger
 
 def add_child_data(event, context):
     logger.info(f"Event: {event}")
-    return_message = None
+    return_message = "customeBody parameter is missing"
 
-    if 'querystring' in event:
+    if 'customBody' in event:
         try:
-            ChildrenHandler.add_child(**event["querystring"])
+            ChildrenHandler.add_child(**event["customBody"])
             return_message = "Child was added successfully"
         except Exception as err:
             logger.error(str(err))
