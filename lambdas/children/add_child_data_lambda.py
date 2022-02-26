6b25ffde3db32125467b7f8218ea398459ab81ec
Dave <keissar3@gmail.com>
2022-02-26 17:38:37 +0200

    fixing logging

--- lambdas/children/add_child_data_lambda.py
+++ lambdas/children/add_child_data_lambda.py
@@ -1,18 +1,19 @@
 import logging
 
 from shared.ChildrenHandler import ChildrenHandler
+from utils.logger import logger
 
 
 def add_child_data(event, context):
-    logging.info(f"Event: {event}")
+    logger.info(f"Event: {event}")
+    return_message = None
 
     if 'querystring' in event:
         try:
             ChildrenHandler.add_child(**event["querystring"])
             return_message = "Child was added successfully"
         except Exception as err:
+            logger.error(str(err))
             return_message = err
 
     return return_message
-
-
