d62274f17e4f721c7aaf714b79d159bffaca8167
rony yosef <61323123+ronyyosef@users.noreply.github.com>
2022-02-26 16:21:37 +0200

    add test and more method to TeacherHandler

--- lambdas/teacher/update_teacher_data_lambda.py
+++ lambdas/teacher/update_teacher_data_lambda.py
@@ -1,7 +1,18 @@
-import logging
+from const import PHONE_NUMBER, FIRST_NAME, LAST_NAME, PHOTO_LINK, KINDERGARTEN_ID, GROUP_NUMBER, IS_ADMIN
+from shared.CognitoHandler import CognitoHandler
+from shared.TeacherHandler import TeacherHandler
 
 
 def update_teacher_data(event, context):
-    logging.info(event)
-
-    return "teacher was added to database (Not really)"
+    teacher_id = CognitoHandler.get_user_id(event)
+    body: dict = event['customBody']
+    teacher_update_info = {
+        PHONE_NUMBER: teacher_id,
+        FIRST_NAME: body.get(FIRST_NAME, None),
+        LAST_NAME: body.get(LAST_NAME, None),
+        PHOTO_LINK: body.get(PHOTO_LINK, None),
+        KINDERGARTEN_ID: body.get(KINDERGARTEN_ID, None),
+        GROUP_NUMBER: body.get(GROUP_NUMBER, None),
+        IS_ADMIN: body.get(IS_ADMIN, None),
+    }
+    TeacherHandler.update_teacher(**teacher_update_info)
