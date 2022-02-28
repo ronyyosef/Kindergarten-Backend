f43957de8e74bf532f17080e3966ce88bab9b605
Dave <keissar3@gmail.com>
2022-02-28 23:08:42 +0200

    adding child no longer getting id as parameter

--- lambdas/children/add_child_data_lambda.py
+++ lambdas/children/add_child_data_lambda.py
@@ -1,6 +1,8 @@
 import logging
 import random
 
+from const import KINDERGARTEN_ID, FIRST_NAME, LAST_NAME, PARENT1_PHONE_NUMBER, PARENT2_PHONE_NUMBER, PHOTO_LINK, \
+    GROUP_NUMBER, ID
 from shared.ChildrenHandler import ChildrenHandler
 from utils.logger import logger
 from utils.random import get_random_id
@@ -8,20 +10,26 @@ from utils.random import get_random_id
 
 def add_child_data(event, context):
     logger.info(f"Event: {event}")
-    return_message = "customBody parameter is missing"
     id_for_added_child = "-1"
 
     id_is_in_use = True
     while id_is_in_use:
         id_for_added_child = get_random_id()
         id_is_in_use = ChildrenHandler.check_if_key_exists(id_for_added_child)
+    logger.info(f"id tobe used {id_for_added_child}")
 
-    if 'customBody' in event:
-        try:
-            ChildrenHandler.add_child(id=id_for_added_child, **event["customBody"])
-            return_message = {"code": "200", "message": "Child updated successfully"}
-        except Exception as err:
-            logger.error(str(err))
-            return_message = err
+    body: dict = event['customBody']
+    child_to_add = {
+        ID: id_is_in_use,
+        KINDERGARTEN_ID: body.get(KINDERGARTEN_ID, None),
+        FIRST_NAME: body.get(FIRST_NAME, None),
+        LAST_NAME: body.get(LAST_NAME, None),
+        PHOTO_LINK: body.get(PHOTO_LINK, None),
+        GROUP_NUMBER: body.get(GROUP_NUMBER, None),
+        PARENT1_PHONE_NUMBER: body.get(PARENT1_PHONE_NUMBER, None),
+        PARENT2_PHONE_NUMBER: body.get(PARENT2_PHONE_NUMBER, None),
+    }
 
-    return return_message
+    logger.info(f"child to be added is :{child_to_add}")
+    ChildrenHandler.add_child(child_to_add)
+    return {"code": "200", "message": f"Child updated successfully with id {id_for_added_child}"}
--- utils/random.py
+++ utils/random.py
@@ -1,8 +1,10 @@
 import random
 
 from const import MAX_ID
+from logger import logger
 
 
 def get_random_id() -> str:
     number = random.randint(1, MAX_ID)
+    logger.info(f"id generated {id}")
     return str(number).zfill(8)
