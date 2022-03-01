a719dc2d9d509ef64dde1b11a77447b9a7c69e0c
Dave <keissar3@gmail.com>
2022-03-01 16:34:39 +0200

    teacher signup completed

--- lambdas/teacher/update_teacher_data_lambda.py
+++ lambdas/teacher/update_teacher_data_lambda.py
@@ -1,11 +1,21 @@
-from const import PHONE_NUMBER, FIRST_NAME, LAST_NAME, PHOTO_LINK, KINDERGARTEN_ID, GROUP_NUMBER, IS_ADMIN
+from const import PHONE_NUMBER, FIRST_NAME, LAST_NAME, PHOTO_LINK, KINDERGARTEN_ID, GROUP_NUMBER, IS_ADMIN, \
+    KINDERGARTEN_NAME
 from shared.CognitoHandler import CognitoHandler
+from shared.KindergartenHandler import KindergartenHandler
 from shared.TeacherHandler import TeacherHandler
+from utils.random import get_random_id
+from utils.logger import logger
 
 
 def update_teacher_data(event, context):
     teacher_id = CognitoHandler.get_user_id(event)
     body: dict = event['customBody']
+
+    if body.get(KINDERGARTEN_ID, None) is None:
+        if KINDERGARTEN_NAME not in body:
+            return "Error If kindergarten_id is null, new kindergarten_name must be provided "
+        body[KINDERGARTEN_ID] = create_kindergarten_for_teacher(body[KINDERGARTEN_NAME])
+
     teacher_update_info = {
         PHONE_NUMBER: teacher_id,
         FIRST_NAME: body.get(FIRST_NAME, None),
@@ -17,3 +27,13 @@ def update_teacher_data(event, context):
     }
     response = TeacherHandler.update_teacher(**teacher_update_info)
     return response
+
+
+def create_kindergarten_for_teacher(kindergarten_name: str) -> str:
+    id_is_in_use = True
+    while id_is_in_use:
+        id_for_added_kindergarten = get_random_id()
+        id_is_in_use = KindergartenHandler.check_if_key_exists(id_for_added_kindergarten)
+    logger.info(f"id to be used {id_for_added_kindergarten}")
+    KindergartenHandler.add_kindergarten(id_for_added_kindergarten, kindergarten_name)
+    return id_for_added_kindergarten
--- shared/KindergartenHandler.py
+++ shared/KindergartenHandler.py
@@ -1,15 +1,26 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
-from const import KINDERGARTEN_TABLE
+from utils.logger import logger
+from const import KINDERGARTEN_TABLE, KINDERGARTEN_NAME, ID
 
-teacher_table = boto3.resource('dynamodb').Table(KINDERGARTEN_TABLE)
+kindergarten_table = boto3.resource('dynamodb').Table(KINDERGARTEN_TABLE)
 
 
 class KindergartenHandler:
 
     @staticmethod
-    def add_kindergarten():
-        pass
+    def add_kindergarten(id: str, kindergarten_name: str):
+
+        new_kindergarten = {
+            ID: id,
+            KINDERGARTEN_NAME: kindergarten_name
+        }
+
+        try:
+            return kindergarten_table.put_item(Item=new_kindergarten)
+        except Exception as e:
+            logger.error(f'Cannot put {new_kindergarten} in {KINDERGARTEN_TABLE}, {str(e)}')
 
     @staticmethod
     def get_kindergarten():
@@ -22,3 +33,9 @@ class KindergartenHandler:
     @staticmethod
     def delete_kindergarten(phone_number):
         pass
+
+    @staticmethod
+    def check_if_key_exists(key_to_search) -> bool:
+        response = kindergarten_table.query(
+            KeyConditionExpression=Key(ID).eq(key_to_search))
+        return len(response["Items"]) > 0
