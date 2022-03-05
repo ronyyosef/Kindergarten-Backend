828ab5d94f3e5a286e89d58291c942d2e68098f0
Rony Yosef <ronyyo@mta.ac.il>
2022-03-05 19:13:33 +0200

    change to generate_presigned_post

--- shared/S3PhotosHandler.py
+++ shared/S3PhotosHandler.py
@@ -11,25 +11,24 @@ class S3PhotosHandler:
     @staticmethod
     def get_photo_url(kindergarten_id, id):
         try:
-            s3_client.head_object(Bucket='kindergarten-photos', Key=f'{kindergarten_id}/{id}.jpg')
+            s3_client.head_object(Bucket='kindergarten-photos', Key=f'{kindergarten_id}/{id}.png')
         except ClientError:
             url = None
         else:
             url = s3_client.generate_presigned_url(
                 ClientMethod='get_object',
-                Params={'Bucket': PHOTOS_BUCKET, 'Key': f'{kindergarten_id}/{id}.jpg'},
+                Params={'Bucket': PHOTOS_BUCKET, 'Key': f'{kindergarten_id}/{id}.png'},
                 ExpiresIn=PRESIGNED_URL_EXPIRE_TIME)
         return url
 
     @staticmethod
     def put_photo_url(kindergarten_id, id):
-        # url = s3_client.generate_presigned_post(Bucket=PHOTOS_BUCKET,
-        #                                         Key=f'{kindergarten_id}/{id}',
-        #                                         Fields={"Content-Type": "image/jpg"},
-        #                                         Conditions=["Content-Range: bytes */1000"],
-        #                                         ExpiresIn=3600)
-        url = boto3.client('s3').generate_presigned_url(
-            ClientMethod='put_object',
-            Params={'Bucket': PHOTOS_BUCKET, 'Key': f'{kindergarten_id}/{id}.jpg'},
-            ExpiresIn=PRESIGNED_URL_EXPIRE_TIME)
+        url = s3_client.generate_presigned_post(
+            Bucket=PHOTOS_BUCKET,
+            Key=f'{kindergarten_id}/{id}.png',
+            ExpiresIn=PRESIGNED_URL_EXPIRE_TIME,
+            Conditions=[
+                ["content-length-range", 1, 10485760]
+            ]
+        )
         return url
