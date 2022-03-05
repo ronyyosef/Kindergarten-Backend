0b3bf073b044752366a4f4a48c281562e5c5f0e6
Rony Yosef <ronyyo@mta.ac.il>
2022-03-05 17:17:21 +0200

    add child_upload_link

--- lambdas/children/get_upload_link_lambda.py
+++ lambdas/children/get_upload_link_lambda.py
@@ -1,3 +1,4 @@
+from const import KINDERGARTEN_ID
 from shared.ChildrenHandler import ChildrenHandler
 from shared.CognitoHandler import CognitoHandler
 from shared.S3PhotosHandler import S3PhotosHandler
@@ -5,10 +6,14 @@ from shared.TeacherHandler import TeacherHandler
 
 
 def get_upload_link(event, context):
-    item_id = event['querystring']['child_id']
-    if not ChildrenHandler.check_if_key_exists(item_id):
-        return 'child id not exist'
-
+    child_id = event['querystring']['child_id']
+    try:
+        child_data = ChildrenHandler.get_child(child_id)
+    except:
+        return 'child does not exist'
+    child_kindergarten_id = child_data[KINDERGARTEN_ID]
     user = CognitoHandler.get_user_id(event)
-    kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(user)
-    return S3PhotosHandler.put_photo_url(kindergarten_id, user)
+    teacher_kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(user)
+    if child_kindergarten_id != teacher_kindergarten_id:
+        return 'child and teacher does not in the same class'
+    return S3PhotosHandler.put_photo_url(child_kindergarten_id, child_id)
