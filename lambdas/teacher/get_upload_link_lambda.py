0954f29a5a714f2cadc1f5acef8a01192e1e0d88
Rony Yosef <ronyyo@mta.ac.il>
2022-03-05 17:28:42 +0200

    add teacher_upload_link

---
+++ lambdas/teacher/get_upload_link_lambda.py
@@ -0,0 +1,9 @@
+from shared.CognitoHandler import CognitoHandler
+from shared.S3PhotosHandler import S3PhotosHandler
+from shared.TeacherHandler import TeacherHandler
+
+
+def get_upload_link(event, context):
+    user = CognitoHandler.get_user_id(event)
+    kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(user)
+    return S3PhotosHandler.put_photo_url(kindergarten_id, user)
