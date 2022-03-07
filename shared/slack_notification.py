cdee7a6b776b5156afdedfd5446ae6bdc68abddf
Rony Yosef <ronyyo@mta.ac.il>
2022-03-07 13:09:48 +0200

    slack alerts for new user

--- shared/slack_notification.py
+++ shared/slack_notification.py
@@ -1,10 +1,17 @@
 import requests
 import os
 
-slack_alert_webhook = os.environ['SLACK_ALERTS_WEBHOOK']
+slack_new_user_alert_webhook = os.environ['SLACK_REGISTER_WEBHOOK']
+slack_errors_alert_webhook = os.environ['SLACK_ALERTS_WEBHOOK']
 
 
-def send_msg(msg):
+def send_new_user_msg(msg):
     payload = '{"text":"%s"}' % msg
-    response = requests.post(slack_alert_webhook,
+    response = requests.post(slack_new_user_alert_webhook,
+                             payload)
+
+
+def send_errors_alert_msg(msg):
+    payload = '{"text":"%s"}' % msg
+    response = requests.post(slack_errors_alert_webhook,
                              payload)
