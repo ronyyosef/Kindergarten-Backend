6f17cdc4236c5ed6cbc57ee68b35c53e0e651ca8
Rony Yosef <ronyyo@mta.ac.il>
2022-03-10 23:28:46 +0200

    fix policy and add stream trigger

--- lambdas/websocket/dynamodb_stream_lambda.py
+++ lambdas/websocket/dynamodb_stream_lambda.py
@@ -1,10 +1,33 @@
 import boto3
-from boto3.dynamodb.table import BatchWriter, logger
 from botocore.exceptions import ClientError
+
+from utils.logger import logger
 
 web_socket_client = boto3.client('apigatewaymanagementapi',
                                  endpoint_url='https://1f8qirmdzh.execute-api.us-east-1.amazonaws.com/dev')
-
+# # MODIFY
+# event = {'Records': [{'eventID': '851163a450805a957c316866fca2a3da', 'eventName': 'MODIFY', 'eventVersion': '1.1',
+#                       'eventSource': 'aws:dynamodb', 'awsRegion': 'us-east-1',
+#                       'dynamodb': {'ApproximateCreationDateTime': 1646949797.0, 'Keys': {'date': {'S': '2022-03-10'},
+#                                                                                          'child_id': {
+#                                                                                              'S': '90e7dca0-3f8c-4713-aee6-a715455b857f'}},
+#                                    'NewImage': {'date': {'S': '2022-03-10'},
+#                                                 'child_id': {'S': '90e7dca0-3f8c-4713-aee6-a715455b857f'},
+#                                                 'kindergarten_id': {'S': 'a949ee75'}, 'time_in': {'S': '18:59:03'},
+#                                                 'ttl': {'N': '1649955555'}, 'time_out': {'S': '19:08:59'}},
+#                                    'SequenceNumber': '9192100000000025472410832', 'SizeBytes': 179,
+#                                    'StreamViewType': 'NEW_IMAGE'},
+#                       'eventSourceARN': 'arn:aws:dynamodb:us-east-1:344089725894:table/AttendanceData/stream/2022-03-10T20:51:31.023'}]}
+# # INSERT
+# event2 = {'Records': [{'eventID': '67220458628aed3fe5748d707795fb37', 'eventName': 'INSERT', 'eventVersion': '1.1',
+#                        'eventSource': 'aws:dynamodb', 'awsRegion': 'us-east-1',
+#                        'dynamodb': {'ApproximateCreationDateTime': 1646950565.0,
+#                                     'Keys': {'date': {'S': '123123213'}, 'child_id': {'S': '123123'}},
+#                                     'NewImage': {'date': {'S': '123123213'}, 'child_id': {'S': '123123'}},
+#                                     'SequenceNumber': '9192300000000025472886698', 'SizeBytes': 54,
+#                                     'StreamViewType': 'NEW_IMAGE'},
+#                        'eventSourceARN': 'arn:aws:dynamodb:us-east-1:344089725894:table/AttendanceData/stream/2022-03-10T20:51:31.023'}]}
+#
 
 def send_message(event, context):
     logger.info(event)
