03494a828cf458f3d5da3c345b3999e294779850
Rony Yosef <ronyyo@mta.ac.il>
2022-03-10 15:37:03 +0200

    remove try except from KindergartenHandler.py

--- shared/hanlders/KindergartenHandler.py
+++ shared/hanlders/KindergartenHandler.py
@@ -11,26 +11,19 @@ class KindergartenHandler:
 
     @staticmethod
     def add_kindergarten(kindergarten_id: str, kindergarten_name: str) -> None:
-
         new_kindergarten = {
             KINDERGARTEN_ID: kindergarten_id,
             KINDERGARTEN_NAME: kindergarten_name
         }
-
-        try:
-            kindergarten_table.put_item(Item=new_kindergarten)
-        except Exception as e:
-            logger.error(f'Cannot put {new_kindergarten} in {KINDERGARTEN_TABLE}, {str(e)}')
+        kindergarten_table.put_item(Item=new_kindergarten)
 
     @staticmethod
     def get_kindergarten(kindergarten_id: str) -> dict:
-        try:
-            logger.info(f'Trying to get kindergarten: {kindergarten_id}')
-            response = kindergarten_table.query(KeyConditionExpression=Key(KINDERGARTEN_ID).eq(kindergarten_id), Limit=1)
-            kindergarten_data = response["Items"][0] if response['Count'] == 1 else None
-            return kindergarten_data
-        except Exception as e:
-            logger.error(f'Error: { str(e)}')
+        logger.info(f'Trying to get kindergarten: {kindergarten_id}')
+        response = kindergarten_table.query(KeyConditionExpression=Key(KINDERGARTEN_ID).eq(kindergarten_id),
+                                            Limit=1)
+        kindergarten_data = response["Items"][0] if response['Count'] == 1 else None
+        return kindergarten_data
 
     @staticmethod
     def update_kindergarten():
