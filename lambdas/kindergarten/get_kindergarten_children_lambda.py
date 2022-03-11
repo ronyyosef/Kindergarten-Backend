4bad1c47167a98a7bbc841ee088220d95588939a
Dave <keissar3@gmail.com>
2022-03-11 15:41:06 +0200

    added attendance to child

--- lambdas/kindergarten/get_kindergarten_children_lambda.py
+++ lambdas/kindergarten/get_kindergarten_children_lambda.py
@@ -1,4 +1,6 @@
+from datetime import date
 from shared.const import TEACHER_ID, CHILD_ID
+from shared.hanlders.AttendanceHandler import AttendanceHandler
 from shared.hanlders.ChildrenHandler import ChildrenHandler
 from shared.hanlders.TeacherHandler import TeacherHandler
 from shared.hanlders.lambda_decorator import lambda_decorator
@@ -8,4 +10,7 @@ from shared.hanlders.lambda_decorator import lambda_decorator
 def get_kindergarten_children(event, context):
     kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(event[TEACHER_ID])
     children = ChildrenHandler.get_children_for_kindergarten(kindergarten_id)
+    for child in children:
+        attend_status = AttendanceHandler.get_attendance(child_id=child.get(CHILD_ID), date_query=str(date.today()))
+        child['is_present'] = attend_status
     return children
