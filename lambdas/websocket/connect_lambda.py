b6a5ed5e47fc3b5d97b4f75a197eac5a2028ab4a
Rony Yosef <ronyyo@mta.ac.il>
2022-03-11 13:01:34 +0200

    change name to WebsocketConnectionManager01

--- lambdas/websocket/connect_lambda.py
+++ lambdas/websocket/connect_lambda.py
@@ -37,7 +37,7 @@ def connect(event, context):
         'cognito:username']
     kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(user_name)
     connectionID = event["requestContext"]["connectionId"]
-    table = boto3.resource('dynamodb').Table('WebsocketConnectionManager')
+    table = boto3.resource('dynamodb').Table('WebsocketConnectionManager01')
     table.put_item(Item={"connection_id": connectionID, 'kindergarten_id': kindergarten_id})
     return {"statusCode": 200, "body": 'connect successfully'}
 
