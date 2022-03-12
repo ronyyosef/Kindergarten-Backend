6e7c4bafd90eaadb85a8e502a9283ae34387cfac
Rony Yosef <ronyyo@mta.ac.il>
2022-03-12 16:56:34 +0200

    change to generate_presigned_post

--- shared/hanlders/S3PhotosHandler.py
+++ shared/hanlders/S3PhotosHandler.py
@@ -1,9 +1,17 @@
+import os
+
 import boto3 as boto3
 from botocore.exceptions import ClientError
 
 from shared.const import PHOTOS_BUCKET, PRESIGNED_URL_EXPIRE_TIME
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3",
+                         region_name="us-east-1",
+                         aws_access_key_id=os.environ[
+                             'S3_UPLOAD_AWS_ACCESS_KEY_ID'],
+                         aws_secret_access_key=os.environ[
+                             'S3_UPLOAD_AWS_SECRET_ACCESS_KEY']
+                         )
 
 
 class S3PhotosHandler:
@@ -25,9 +33,11 @@ class S3PhotosHandler:
 
     @staticmethod
     def put_photo_url(kindergarten_id: str, id: str) -> str:
-        url = s3_client.generate_presigned_url(
-            ClientMethod='put_object',
-            Params={'Bucket': PHOTOS_BUCKET,
-                    'Key': f'{kindergarten_id}/{id}.png'},
-            ExpiresIn=PRESIGNED_URL_EXPIRE_TIME)
-        return url
+        response = s3_client.generate_presigned_post(
+            Bucket=PHOTOS_BUCKET,
+            Key=f'{kindergarten_id}/{id}.png',
+            ExpiresIn=PRESIGNED_URL_EXPIRE_TIME,
+            Conditions=[
+                ["content-length-range", 1,
+                 5120000]])
+        return response
