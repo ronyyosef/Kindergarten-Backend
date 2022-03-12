9258d1038cc8f8d41b241120d0e1963574a3d677
Dave <keissar3@gmail.com>
2022-03-12 22:50:29 +0200

    Adding test to spreadsheet

--- shared/hanlders/SpreadsheetHandler.py
+++ shared/hanlders/SpreadsheetHandler.py
@@ -13,37 +13,34 @@ class SpreadsheetHandler:
         kindergarten_data = KindergartenHandler.get_kindergarten(kindergarten_id)
         kindergarten_id = kindergarten_data["kindergarten_id"]
 
-        columns = ["child_name", "day", "time_in", "time_out", "total_stay", "ate_pizza"]
         children = ChildrenHandler.get_children_for_kindergarten(kindergarten_id, )
         report_generated = {}
         for child in children:
             child_name = f'{child["first_name"]} {child["last_name"]}'
-            report_generated[child_name] = ""
-
-            child["monthly_attendance_report_csv"] = []
+            child_id = child["child_id"]
+            report_generated[child_id] = "child_name, day, time_in, time_out, total_stay, ate_pizza\n"
             child["monthly_attendance_report"] = AttendanceHandler.get_attendance_for_entire_month(child["child_id"],
                                                                                                    month)
 
             if child["monthly_attendance_report"] is None:
                 child["monthly_attendance_report"] = []
             for attendance in child["monthly_attendance_report"]:
-                time_in = ""
-                time_out = ""
+                time_in = attendance.get("time_in", None)
+                time_out = attendance.get("time_out", None)
                 total_stay = ""
                 ate_pizza = ""
-                day = ""
-                time_in_obj_data = attendance["time_in"]
-                time_out_obj_data = attendance["time_out"]
-                if time_in_obj_data and time_out_obj_data:
-                    time_in_obj = datetime.strptime(time_in_obj_data, "%H:%M:%S")
-                    time_out_obj = datetime.strptime(time_out_obj_data, "%H:%M:%S")
+                day = attendance.get("date", None)
+
+                if time_in and time_out:
+                    time_in_obj = datetime.strptime(time_in, "%H:%M:%S")
+                    time_out_obj = datetime.strptime(time_out, "%H:%M:%S")
                     if time_out_obj > time_in_obj:
                         total_stay = str(time_out_obj - time_in_obj)
-                day = attendance.get("date", "")
+                else:
+                    time_in = time_out = ""
 
-                report_generated[child_name] += (','.join(
-                    [child_name, day, time_in, time_out, total_stay, ate_pizza])) + "\n"
-
-                # df_children_list.append(df_child.to_csv())
+                report_generated[child_id] += (','.join(
+                    [child_name, day, time_in, time_out, total_stay,
+                     ate_pizza])) + "\n"
 
         return report_generated
