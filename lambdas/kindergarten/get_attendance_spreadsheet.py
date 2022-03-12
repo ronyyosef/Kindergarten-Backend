5d9f3f92e7b9fd341c360f0c3f7e9c1e1f2a77a3
Dave <keissar3@gmail.com>
2022-03-12 18:07:30 +0200

    Adding test to spreadsheet

--- lambdas/kindergarten/get_attendance_spreadsheet.py
+++ lambdas/kindergarten/get_attendance_spreadsheet.py
@@ -9,4 +9,4 @@ def get_attendance_spreadsheet(event, context):
     kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(event[TEACHER_ID])
     attendance_report_data = SpreadsheetHandler.get_kindergarten_spreadsheet(kindergarten_id=kindergarten_id,
                                                                              month=event['customBody']['month'])
-    return attendance_report_data
+    return "test"
