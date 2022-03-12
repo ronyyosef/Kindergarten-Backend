a6feede2bc21697a6f85733b0f1f674751d19b2f
Rony Yosef <ronyyo@mta.ac.il>
2022-03-12 21:35:59 +0200

    change get_attendance_spreadsheet.py to use querystring

--- lambdas/kindergarten/get_attendance_spreadsheet.py
+++ lambdas/kindergarten/get_attendance_spreadsheet.py
@@ -8,5 +8,5 @@ from shared.hanlders.lambda_decorator import lambda_decorator
 def get_attendance_spreadsheet(event, context):
     kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(event[TEACHER_ID])
     attendance_report_data = SpreadsheetHandler.get_kindergarten_spreadsheet(kindergarten_id=kindergarten_id,
-                                                                             month=event['customBody']['month'])
-    return "test"
+                                                                             month=event['querystring']['month'])
+    return attendance_report_data
