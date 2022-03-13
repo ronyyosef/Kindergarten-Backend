1f4456b6c266cf58ec8147f812f7d37787b95a82
Dave <keissar3@gmail.com>
2022-03-13 12:26:51 +0200

    Adding test to spreadsheet

--- lambdas/signup/signup_teacher_lambda.py
+++ lambdas/signup/signup_teacher_lambda.py
@@ -17,7 +17,9 @@ def signup_teacher(event, context):
             return "Error If kindergarten_id is null, new kindergarten_name must be provided"
         body[KINDERGARTEN_ID] = create_kindergarten_for_teacher(
             body[KINDERGARTEN_NAME])
-    # TODO make sure kindergarten_id exist
+
+    if KindergartenHandler.check_if_kindergarten_exists(body[KINDERGARTEN_ID]) is False:
+        raise Exception(f"Kindergarten with id: {body[KINDERGARTEN_ID]} does not exist")
 
     teacher_update_info = {
         TEACHER_ID: event[TEACHER_ID],
@@ -32,6 +34,7 @@ def signup_teacher(event, context):
 
 def create_kindergarten_for_teacher(kindergarten_name: str) -> str:
     id_is_in_use = True
+    id_for_added_kindergarten = None
     while id_is_in_use:
         id_for_added_kindergarten = get_random_id()
         id_is_in_use = KindergartenHandler.check_if_kindergarten_exists(
