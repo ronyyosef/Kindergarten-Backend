26408a3e9a2710fb6d177caecf2f6ea1042b2644
Rony Yosef <ronyyo@mta.ac.il>
2022-03-14 11:21:24 +0200

    fix data validator

--- lambdas/attendance/add_attendance_data_lambda.py
+++ lambdas/attendance/add_attendance_data_lambda.py
@@ -43,35 +43,42 @@ def add_attendance_data(event, context):
             TIME_IN, None), is_present=child_attendance.get(
             IS_PRESENT, None))
 
+# class Data(BaseModel):
+#     id: str
+#     p: str
+#
+#     @validator('id','p')
+#     def validate_id(cls,v):
+#         return v
+#
+# x = Data(**{'id':'20','p':'30'})
+# print(x)
+
 
 class InputData(BaseModel):
-    child_id: str = Field(..., alias=ID)
-    is_present: str = Field(..., alias=IS_PRESENT)
-    teacher_id: str = Field(..., alias='teacher_id')
+    child_id: str
+    is_present: str
+    teacher_id: str
     kindergarten_id: str
 
     @root_validator(pre=True)
     @classmethod
-    def _extract_data(cls, values):
+    def validate_data(cls, values):
         try:
-            values[ID] = values[EVENT_BODY][ID]
+            values[CHILD_ID] = values[EVENT_BODY][ID]
             values[IS_PRESENT] = values[EVENT_BODY][IS_PRESENT]
+            values[
+                KINDERGARTEN_ID] = TeacherHandler.get_teacher_kindergarten_id(
+                values[TEACHER_ID])
+
+            if values[IS_PRESENT] != 'yes' and values[IS_PRESENT] != 'no':
+                raise MyException("is_present should be: yes|no", INPUT_ERROR)
+
+            if not ChildrenHandler.child_in_kindergarten(
+                    values[CHILD_ID], values[KINDERGARTEN_ID]):
+                raise MyException(
+                    'child not exist/ child not in this kindergarten',
+                    INPUT_ERROR)
         except BaseException:
             raise MyException("Input Error, parameter is missing", INPUT_ERROR)
-        return values
-
-    @root_validator
-    def is_present(cls, values):
-        if values[IS_PRESENT] != 'yes' and values[IS_PRESENT] != 'no':
-            raise MyException("is_present should be: yes|no", INPUT_ERROR)
-
-    @root_validator
-    def child_in_kindergarten(cls, values):
-        values[KINDERGARTEN_ID] = TeacherHandler.get_teacher_kindergarten_id(
-            values[TEACHER_ID])
-        if not ChildrenHandler.child_in_kindergarten(
-                values[CHILD_ID], values[KINDERGARTEN_ID]):
-            raise MyException(
-                'child not exist/ child not in this kindergarten',
-                INPUT_ERROR)
         return values
