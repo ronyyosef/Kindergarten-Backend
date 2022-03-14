ebb4b88f5438da2fe846d1a28df8951483e7902f
Dave <keissar3@gmail.com>
2022-03-14 11:35:24 +0200

    Adding test to spreadsheet

--- tests/test_children_handler.py
+++ tests/test_children_handler.py
@@ -1,8 +1,5 @@
-# from datetime import date
-
-# from shared.AttendanceHandler import ChildrenHandler
-
 from shared.hanlders.ChildrenHandler import ChildrenHandler
+from utils.random import get_random_id
 
 
 def setup_module():
@@ -32,3 +29,29 @@ def test_add_child_handler():
         'parent2_phone_number': 'test_parent2_phone_number',
         'parent1_phone_number': 'test_parent1_phone_number',
         'photo_link': None}
+
+
+def test_delete_child_handler():
+    test_child_id = get_random_id()
+    ChildrenHandler.add_child(child_id=test_child_id,
+                              kindergarten_id='test_kindergarten_id',
+                              first_name='test_first_name',
+                              last_name='test_last_name',
+                              parent1_phone_number='test_parent1_phone_number',
+                              parent2_phone_number='test_parent2_phone_number',
+                              group_number='test_group_number',
+                              )
+    response = ChildrenHandler.get_child(child_id=test_child_id)
+    assert response == {
+        'child_id': test_child_id,
+        'last_name': 'test_last_name',
+        'group_number': 'test_group_number',
+        'kindergarten_id': 'test_kindergarten_id',
+        'first_name': 'test_first_name',
+        'parent2_phone_number': 'test_parent2_phone_number',
+        'parent1_phone_number': 'test_parent1_phone_number',
+        'photo_link': None}
+
+    ChildrenHandler.delete_child(child_id=test_child_id, kindergarten_id=response["kindergarten_id"])
+    response = ChildrenHandler.get_child(child_id=test_child_id)
+    assert response is None
