376a76d3061b978d293b1d10560ed487199092b6
Dave <keissar3@gmail.com>
2022-03-20 21:56:00 +0200

    changing delete to post

--- lambdas/children/delete_child_data_lambda.py
+++ lambdas/children/delete_child_data_lambda.py
@@ -1,12 +1,12 @@
 from shared.const import TEACHER_ID, EVENT_BODY
 from shared.hanlders.ChildrenHandler import ChildrenHandler
 from shared.hanlders.TeacherHandler import TeacherHandler
+from shared.hanlders.lambda_decorator import lambda_decorator
 from utils.logger import logger
 
 
+@lambda_decorator
 def delete_child_data(event, context):
-    logger.info("Starting delete child data ")
-    logger.info(f"event :{event}")
     teacher_data = TeacherHandler.get_teacher_data(event[TEACHER_ID])
     body: dict = event[EVENT_BODY]
     child_id_to_delete = body.get("id")
