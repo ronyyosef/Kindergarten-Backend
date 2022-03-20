a4f58686407bd92fac82d92a554ac9630260da89
Dave <keissar3@gmail.com>
2022-03-20 21:24:48 +0200

    changing delete to post

--- lambdas/children/delete_child_data_lambda.py
+++ lambdas/children/delete_child_data_lambda.py
@@ -1,12 +1,13 @@
-from shared.const import TEACHER_ID
+from shared.const import TEACHER_ID, EVENT_BODY
 from shared.hanlders.ChildrenHandler import ChildrenHandler
 from shared.hanlders.TeacherHandler import TeacherHandler
 
 
 def delete_child_data(event, context):
     teacher_data = TeacherHandler.get_teacher_data(event[TEACHER_ID])
-    child_id_to_delete = event["querystring"]["id"]
-    child = ChildrenHandler.get_child(event["querystring"]["id"])
+    body: dict = event[EVENT_BODY]
+    child_id_to_delete = body.get("id")
+    child = ChildrenHandler.get_child(child_id_to_delete)
     if child["kindergarten_id"] != teacher_data["kindergarten_id"]:
         raise Exception(
             f'child id {child_id_to_delete} cant be deleted by teacher in kindergarten{teacher_data["kindergarten_id"]}')
