12a0a0b4af2ebf17ff751b203f8bdf9cc7d5ea3e
Rony Yosef <ronyyo@mta.ac.il>
2022-03-25 21:41:31 +0300

    fix tests

--- tests/test_groups_handler.py
+++ tests/test_groups_handler.py
@@ -1,14 +1,17 @@
 from shared.hanlders.GroupsHandler import GroupsHandler
 from utils.random import get_random_id
 
-test_kindergarten_id = "f5bb3f4b"
+test_kindergarten_id = "test"
 
 
 def test_add_groups():
-    random_id = get_random_id()
-    GroupsHandler.add_group_to_kindergarten(test_kindergarten_id, random_id)
+    test_group_name = "test_group_name"
+    GroupsHandler.add_group_to_kindergarten(
+        kindergarten_id=test_kindergarten_id,
+        group_name=test_group_name)
     response = GroupsHandler.get_kindergarten_groups(test_kindergarten_id)
-    assert random_id in response["groups_in_kindergarten"], f"Group with name {random_id} failed to create"
+    assert test_group_name in response[
+        "groups_in_kindergarten"], f"Group with name {test_group_name} failed to create"
 
 
 def test_get_groups():
@@ -20,7 +23,10 @@ def test_delete_groups():
     random_id = get_random_id()
     GroupsHandler.add_group_to_kindergarten(test_kindergarten_id, random_id)
     response = GroupsHandler.get_kindergarten_groups(test_kindergarten_id)
-    assert random_id in response["groups_in_kindergarten"], f"Group with name {random_id} failed to create"
-    GroupsHandler.delete_group_from_kindergarten(test_kindergarten_id, random_id)
+    assert random_id in response[
+        "groups_in_kindergarten"], f"Group with name {random_id} failed to create"
+    GroupsHandler.delete_group_from_kindergarten(
+        test_kindergarten_id, random_id)
     response = GroupsHandler.get_kindergarten_groups(test_kindergarten_id)
-    assert random_id not in response["groups_in_kindergarten"], f"Group with name {random_id} failed to delete"
+    assert random_id not in response[
+        "groups_in_kindergarten"], f"Group with name {random_id} failed to delete"
