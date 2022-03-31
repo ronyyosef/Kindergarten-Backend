3270082ecffe5f1e44d5cf36d5269e542a688c82
Rony Yosef <ronyyo@mta.ac.il>
2022-03-31 23:59:42 +0300

    update delete_kindergarten_group logic

--- lambdas/groups/delete_kindergarten_group_lambda.py
+++ lambdas/groups/delete_kindergarten_group_lambda.py
@@ -8,23 +8,22 @@ from shared.hanlders.TeacherHandler import TeacherHandler
 from shared.hanlders.lambda_decorator import lambda_decorator
 
 
-def group_is_empty(teacher_data: dict) -> bool:
+def group_is_empty(teacher_data: dict, group_name: str) -> bool:
     children = ChildrenHandler.get_children_for_kindergarten_and_group_no_photo_link(
-        kindergarten_id=teacher_data.get(KINDERGARTEN_ID),
-        group_name=teacher_data.get(GROUP_NAME))
-    teachers = TeacherHandler.get_all_teacher_in_group(teacher_data)
+        kindergarten_id=teacher_data[KINDERGARTEN_ID], group_name=group_name)
+    teachers = TeacherHandler.get_all_teacher_in_group(teacher_data,group_name)
     return bool(len(children) == 0) and bool(len(teachers) == 0)
 
 
 @lambda_decorator
 def delete_kindergarten_group(event, context):
     teacher_data = TeacherHandler.get_teacher_data(event[TEACHER_ID])
-    kindergarten_id = teacher_data.get("kindergarten_id")
+    kindergarten_id = teacher_data.get(KINDERGARTEN_ID)
     body: dict = event[EVENT_BODY]
-    group_name_to_delete = body["group_name"]
+    group_name_to_delete = body[GROUP_NAME]
     if group_name_to_delete == MAIN_GROUP:
         raise MyException("The main group cannot be deleted", INPUT_ERROR)
-    if group_is_empty(teacher_data) is True:
+    if group_is_empty(teacher_data, group_name_to_delete) is True:
         GroupsHandler.delete_group_from_kindergarten(kindergarten_id,
                                                      group_name_to_delete)
     else:
