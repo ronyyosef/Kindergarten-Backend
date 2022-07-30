e4663d8470514b84786818062906b416769c3e3f
Rony Yosef <ronyyo@mta.ac.il>
2022-07-30 13:05:20 +0300

    add parent

--- lambdas/parent/add_child_attendance_lambda.py
+++ lambdas/parent/add_child_attendance_lambda.py
@@ -1,5 +1,5 @@
 # get parameter of date and child and update its attendance on spastic date
-from shared.const import CHILD_ID, DATE, IS_PRESENT, KINDERGARTEN_ID
+from shared.const import CHILD_ID, DATE, IS_PRESENT, KINDERGARTEN_ID, EVENT_BODY
 from shared.hanlders.AttendanceHandler import AttendanceHandler
 from shared.hanlders.lambda_decorator import lambda_decorator
 
@@ -7,10 +7,10 @@ from shared.hanlders.lambda_decorator import lambda_decorator
 @lambda_decorator
 def add_child_attendance(event, context):
     return {
-        DATE: event[DATE],
-        CHILD_ID: event[CHILD_ID],
-        IS_PRESENT: event[IS_PRESENT],
-        KINDERGARTEN_ID: event[KINDERGARTEN_ID]}
+        DATE: event[EVENT_BODY][DATE],
+        CHILD_ID: event[EVENT_BODY][CHILD_ID],
+        IS_PRESENT: event[EVENT_BODY][IS_PRESENT],
+        KINDERGARTEN_ID: event[EVENT_BODY][KINDERGARTEN_ID]}
     date = event[DATE]
     child_id = event[CHILD_ID]
     is_present = event[IS_PRESENT]
