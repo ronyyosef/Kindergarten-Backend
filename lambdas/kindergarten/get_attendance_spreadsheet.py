22d48e6e3e235539c4af761592a50de54d905ac9
Dave <keissar3@gmail.com>
2022-08-02 19:45:06 +0300

    upload to s3

--- lambdas/kindergarten/get_attendance_spreadsheet.py
+++ lambdas/kindergarten/get_attendance_spreadsheet.py
@@ -1,15 +1,29 @@
-import csv, io
-from requests.models import Response
+import csv
+import logging
+import os
+# from shared.hanlders.lambda_decorator import lambda_decorator
+from datetime import datetime
+
+import boto3 as boto3
+from botocore.exceptions import ClientError
+
+from shared.const import PHOTOS_BUCKET, PRESIGNED_URL_EXPIRE_TIME
 from shared.const import TEACHER_ID, EVENT_QUERY_STRING
 from shared.hanlders.SpreadsheetHandler import SpreadsheetHandler
 from shared.hanlders.TeacherHandler import TeacherHandler
-from shared.hanlders.lambda_decorator import lambda_decorator
-from datetime import datetime
-from shared.const import MARCH, MAY, IS_PRESENT
-from datetime import date
+
+s3_client = boto3.client("s3",
+                         region_name="us-east-1",
+                         aws_access_key_id=os.environ[
+                             'S3_UPLOAD_AWS_ACCESS_KEY_ID'],
+                         aws_secret_access_key=os.environ[
+                             'S3_UPLOAD_AWS_SECRET_ACCESS_KEY']
+                         )
 
 
-@lambda_decorator
+
+
+#@lambda_decorator
 def get_attendance_spreadsheet(event, context):
     kindergarten_id = TeacherHandler.get_teacher_kindergarten_id(event[TEACHER_ID])
 
@@ -24,7 +38,39 @@ def get_attendance_spreadsheet(event, context):
         writer = csv.writer(f)
         writer.writerows(attendance_report_data)
 
-    # upload file to s3 here.
-    # delete file from local storage
-    return "s3 link to file"
+    upload_file("out.csv", PHOTOS_BUCKET, f"{event[TEACHER_ID]}.csv")
+    if os.path.isfile("out.csv"):
+        os.remove("out.csv")
 
+    url = s3_client.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={'Bucket': PHOTOS_BUCKET,
+                'Key': f"{event[TEACHER_ID]}.csv"},
+        ExpiresIn=PRESIGNED_URL_EXPIRE_TIME)
+
+    return url
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+get_attendance_spreadsheet({TEACHER_ID:"03118409-b0f2-4464-94eb-93d2cd728b16",EVENT_QUERY_STRING:{"month":'8'}},{})
